9b6a68cbcf49def1212e42dee8b1107cf521ac11
Jared R. Luellen <jared.luellen@cs.olivetcollege..edu>
2013-12-15 23:11:09 -0500

    testing

--- sms_ip.py
+++ sms_ip.py
@@ -25,26 +25,24 @@ for i in interfaces:
             connection = True
 
 if connection == True:
-	try:
-		# Reads a file listed in git ignore, it contains username, password, and phone number(s) you wish to send to
-		# EXAMPLE user_info.txt file:
-		# email@gmail.com
-		# YOUR_PASSWORD
-		# 3333333333, 3333333333, 3333333333
+        try:
+                # Reads a file listed in git ignore, it contains username, password, and phone number(s) you wish to send to
+                # EXAMPLE user_info.txt file:
+                # email@gmail.com
+                # YOUR_PASSWORD
+                # 3333333333, 3333333333, 3333333333
 
-		# MUST SET ABSOLUTE PATH FOR user_info.txt 
-		print '/home/pi/sms_ip/user_info.txt'
-		user_info = ('/home/pi/sms_ip/user_info.txt', 'r')
-		user_name = user_info.readline()
-		user_pass = user_info.readline()
-		user_tele = user_info.readline()
-		user_info.close()
-		voice.login(user_name, user_pass)
-		phoneNumber = user_tele
-		text = 'RaspberryPi\n\n'+'IP: '+str(ip_addr)
-		print len(text)
-	except:
-		print 'failed to send IP'
-		sys.exit(1)
-	finally:
-		voice.send_sms(phoneNumber, text)
+                # MUST SET ABSOLUTE PATH FOR user_info.txt 
+                user_info = open('/home/pi/sms_ip/user_info.txt', 'r')
+                user_name = user_info.readline()
+                user_pass = user_info.readline()
+                user_tele = user_info.readline()
+                user_info.close()
+                voice.login(user_name, user_pass)
+                phoneNumber = user_tele
+                text = 'RaspberryPi\n\n'+'IP: '+str(ip_addr)
+                print len(text)
+                voice.send_sms(phoneNumber, text)
+        except:
+                print 'failed to send IP'
+                sys.exit(1)
